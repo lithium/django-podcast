e1d8b3902fb93aff88b09fddf92aee9c7bf7f855
lithium <code@hlidskialf.com>
2015-06-21 21:01:19 -0700

    fork

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ packages = find_packages('.')
 
 setup(
     name='django-podcast',
-    version='0.5.0',
-    description='Django podcast app',
+    version='0.6.0',
+    description='lithium\'s fork of django-podcast',
     packages=packages,
     package_data=dict((package_name, template_patterns) for package_name in packages),
-    url='https://github.com/jefftriplett/django-podcast',
+    url='https://github.com/lithium/django-podcast',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
